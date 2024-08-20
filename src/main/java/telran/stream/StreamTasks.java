cbe36cabde4cbd21371e51d411bc267805e71edf
Artem Khazanov <tulenka04@gmail.com>
2024-08-20 14:17:54 +0300

    update stream operations

--- src/main/java/telran/stream/StreamTasks.java
+++ src/main/java/telran/stream/StreamTasks.java
@@ -1,24 +1,9 @@
 package telran.stream;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 public class StreamTasks {
     public static int[] shuffle(int[] array) {
-        //TODO
-        //only one pipline for getting new array of int's
-        //with all numbers from a given array but with different order
-        //each method call returns new array in some random order
-        // return Arrays.stream(array).boxed().map(n -> array[new Random().nextInt(array.length)]).mapToInt(Integer::intValue).toArray();
-        return Arrays.stream(array).boxed().map(i -> {
-            int j = new Random().nextInt(array.length);
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            return array[i];
-        }).mapToInt(Integer::intValue).toArray();
+        return new Random().ints(0, array.length).distinct().limit(array.length)
+    .map(i -> array[i]).toArray();
     }
 }
--- src/test/java/telran/stream/StreamTasksTest.java
+++ src/test/java/telran/stream/StreamTasksTest.java
@@ -1,26 +1,31 @@
 package telran.stream;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 public class StreamTasksTest {
+    private static final int N_RUNS = 100;
+
     @Test
     void shuffleTest() {
-        //TODO
-        //testing of method shuffle of StreamTasks class
-        int[] array = {1,2,3,4,5,6};
-        int[] expected = {6,5,4,3,2,1};
-        System.out.println(Arrays.toString(StreamTasks.shuffle(array)));
-        System.out.println(Arrays.toString(StreamTasks.shuffle(array)));
-        System.out.println(Arrays.toString(StreamTasks.shuffle(array)));
-        System.out.println(Arrays.toString(StreamTasks.shuffle(array)));
-        // assertArrayEquals(expected, StreamTasks.shuffle(array));
-        // assertNotEquals(expected, StreamTasks.shuffle(array));
+        int array[] = { 10, 10, 20, 30, 40, 50, 50, 70, 80, 90 };
+        int[] arShuffled = StreamTasks.shuffle(array);
+        for (int i = 1; i < N_RUNS; i++) {
+            int[] arShuffled2 = StreamTasks.shuffle(array);
+            assertFalse(Arrays.equals(arShuffled, arShuffled2));
+            arShuffled = arShuffled2;
+            runTest(arShuffled, array);
 
+        }
+    }
+
+    private void runTest(int[] shuffled, int[] array) {
+        assertFalse(Arrays.equals(shuffled, array));
+        Arrays.sort(shuffled);
+        assertTrue(Arrays.equals(shuffled, array));
     }
 
 }
